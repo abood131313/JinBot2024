b285a3734f326e6f259b92e7d29bd795284261a2
Matt <89090273+xJinMori31@users.noreply.github.com>
2024-06-09 14:31:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,23 +11,29 @@ bot.on('spawn', () => {
   bot.chat('/register contraseña')  
 });
 
+//NO TOCAR/// DO NOT TOUCH
+
 bot.on("move", function() {
   //triggers when the bot moves
+  //DONT MODIFY THE CODE, THIS CODE WAS CREATED BY JINMORI (YOUTUBE @JIMORIYT). READ THE LICENSE.
 
   bot.setControlState("jump", true); //continuously jumps
   setTimeout(() => {
     //sets a delay
     bot.setControlState("jump", false); //stops jumping
   }, 1000); //delay time
+  //DONT MODIFY THE CODE, THIS CODE WAS CREATED BY JINMORI (YOUTUBE @JIMORIYT). READ THE LICENSE.
 
   setTimeout(() => {
     //sets a delay
+    //DONT MODIFY THE CODE, THIS CODE WAS CREATED BY JINMORI (YOUTUBE @JIMORIYT). READ THE LICENSE.
     bot.setControlState("forward", true); //continuously walks forward
     setTimeout(() => {
       //sets a delay
       bot.setControlState("forward", false); //stops walking forward
     }, 500); //delay time
   }, 1000); //delay time
+  //DONT MODIFY THE CODE, THIS CODE WAS CREATED BY JINMORI (YOUTUBE @JIMORIYT). READ THE LICENSE.
 
   setTimeout(() => {
     //sets a delay
@@ -40,6 +46,7 @@ bot.on("move", function() {
 
   setTimeout(() => {
     //sets a delay
+    //DONT MODIFY THE CODE, THIS CODE WAS CREATED BY JINMORI (YOUTUBE @JIMORIYT). READ THE LICENSE.
     bot.setControlState("right", true); //continuously walks right
     setTimeout(() => {
       //sets a delay
@@ -56,6 +63,7 @@ bot.on("move", function() {
     }, 2000); //delay time
   }, 500); //delay time
 });
+  //DONT MODIFY THE CODE, THIS CODE WAS CREATED BY JINMORI (YOUTUBE @JIMORIYT). READ THE LICENSE.
 
 bot.on('kicked', console.log)
 bot.on('error', console.log)
@@ -63,3 +71,6 @@ bot.on('end', createBot)
 }
 
 createBot()
+
+
+//DONT MODIFY THE CODE, THIS CODE WAS CREATED BY JINMORI (YOUTUBE @JIMORIYT). READ THE LICENSE.
